9327cbbde8accbe2878662f9ab48fa04312e8c9b
juanmanoel237 <ndeguemanoeljuan@gmail.com>
2024-09-12 20:07:14 +0200

    Fin du premier endpoint pour récupérer les images

--- app.js
+++ app.js
@@ -1,9 +1,49 @@
 import express from 'express'
 import axios from 'axios'
-import cheero from 'cheero'
+import * as cheerio from 'cheerio'
 
 const app = express()
 const port = 3000
+
+// Rest of your code...
+
+
+let processedUrls = []
+
+//Endpoint pour récupérer les images
+
+app.get('/images', async (req, res)=>{
+    const siteUrl = req.query.url
+
+    if(!siteUrl){
+        return res.status(400).json({error:'Veuillez donner une URL'})
+    }
+
+    try{
+        const {data} = await axios.get(siteUrl)
+
+        const $ = cheerio.load(data)
+
+        const images = []
+
+        $('img').each((index, element)=>{
+            const src = $(element).attr('src')
+
+            if(src){
+                images.push(src)
+            }
+        })
+
+        if(!processedUrls.includes(siteUrl)){
+            processedUrls.push(siteUrl)
+        }
+
+        return res.json({images})
+    }
+    catch(err){
+        return res.status(500).json({err : 'Impossible de rcupérer les images'})
+    }
+})
 
 app.listen(port, ()=>{
     console.log('Serveur démarré !!');
